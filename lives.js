82eb7e582820c162feb81f9730c1d7876ace3f82
Jarquevious <jarquevious@gmail.com>
2021-02-01 17:26:35 -0800

    lives class updated

--- lives.js
+++ lives.js
@@ -1,4 +1,28 @@
 class Lives {
-    constructor() 
+    constructor(x, y, color, lives, font) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.lives = lives;
+        this.font = font;
+    }
+    render(ctx) {
+            ctx.font = "16px Arial";
+            ctx.fillStyle = "#0095DD";
+            ctx.fillText("Lives: " + this.lives, canvas.width - 65, 20);
+        }
+
+    }
+    looselife() {
+        this.lives--;
+        if(!this.lives) {
+        this.reset()    
+    }
+
+    reset() {
+        alert("GAME OVER");
+        document.location.reload();
+
+    }
 
 }
